e8b853472c1f9a39ae9b9a49ecda0fa199101f45
jaimathur <mathurjai88@gmail.com>
2024-11-24 15:26:33 +0530

    simulation added

---
+++ experiment/simulation/js/data.js
@@ -0,0 +1,9 @@
+//all variables
+let pressure = 0;
+let aria_ratio_denominator = Math.round(Math.random() * 20 + 90);
+let ratio = 1 / aria_ratio_denominator;
+let h1 = parseFloat((Math.random() * 0.05 + 0.2).toFixed(2));
+let h2 = parseFloat((Math.random() * 0.1 + 0.4).toFixed(2));
+let rho1 = 800;
+let rho2 = 1000 * 13.6;
+//# sourceMappingURL=data.js.map
